e521a817e8a8cee551e09c65e1af3538f2200048
mnishio1 <mnishio1@lenovo.com>
2023-12-04 09:51:24 +0900

    Chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,5 +1,6 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { notFound } from 'next/navigation';
 // import { fetchCustomers } from '@/app/lib/data';
 
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
@@ -9,6 +10,9 @@ export default async function Page({ params }: { params: { id: string } }) {
         fetchInvoiceById(id),
         fetchCustomers(),
       ]);
+      if (!invoice) {
+        notFound();
+      }
 // export default async function Page() {
   return (
     <main>
